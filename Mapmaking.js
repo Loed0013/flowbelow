67e487dfe79c85f69e6b306bef42a0084455a715
Loed0013 <94440181+Loed0013@users.noreply.github.com>
2021-11-17 11:16:59 +0100

    Update Mapmaking.js

--- Mapmaking.js
+++ Mapmaking.js
@@ -13,9 +13,5 @@
   var marker1= L.marker([63.8250972,20.3109672]).addTo(mymap);
   var marker6= L.marker([63.8360593,20.3163476]).addTo(mymap);
 
-  function onMarkerClick(e) {
-    alert("You clicked on a marker");
-      }
-      marker1.on('click', onMarkerClick);
-      marker6.on('click', onMarkerClick);
-      marker6.bindPopup("<a href=sensor6.html>Sensor6</a>").openPopup();
+
+  marker6.bindPopup("<a href=sensor6.html>Sensor6</a>").openPopup();
