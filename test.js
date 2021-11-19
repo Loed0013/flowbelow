42331c448e0fdcc65e7d7c7c7774aab5f865ef8e
Loed0013 <94440181+Loed0013@users.noreply.github.com>
2021-11-19 11:46:27 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,49 @@
+chartIt();
+async function chartIt(){
+  const data = await getLevelData();
+  const ctx = document.getElementById('Chart').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: data.Time,
+        datasets: [{
+            label: 'Exempel på sensordata',
+            data: data.Level,
+            backgroundColor: ['rgba(255, 99, 132, 0.2)',  ],
+
+            borderColor: ['rgba(255, 99, 132, 1)',],
+
+            borderWidth: 1
+        }]
+    },options: {
+        onClick: (e) => {
+          const canvasPosition = Chart.helpers.getRelativePosition(e, chart);
+
+          // Substitute the appropriate scale IDs
+          const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+          const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+        }
+
+      //Länk med mer info:  https://www.chartjs.org/docs/latest/configuration/interactions.html#event-option
+  }
+  });
+}
+
+
+  async function getLevelData(){
+    const Time = [];
+    const Level = [];
+    const response = await fetch ('Data34.csv');
+    const data = await response.text();
+
+    const table = data.split('\n');
+    for(let i = 0;i<table.length-1;i++){
+      const cols = table[i].split(',');
+      const ts = cols[0];
+      const ls = cols[1];
+
+      Time.push(ts);
+      Level.push(ls);
+    }
+    return {Time,Level};
+  }
